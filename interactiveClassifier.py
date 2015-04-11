5db398c51ca1a92281483f7440f90435eb3fcb81
Duncan <dac80@zips.uakron.edu>
2015-04-11 18:50:30 -0400

    Changed

---
+++ interactiveClassifier.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Automated Classifier
+Shows the gui which runs a Demo to run the classifier code live
+Created by Duncan Campbell
+"""
+
+import numpy as np
+import cv2 as cv
+
+cv.namedWindow('Image Classifie')
--- trainer.py
+++ trainer.py
@@ -20,9 +20,9 @@ import numpy as np
 print 'Initializing the Neural Network'
 network = FeedForwardNetwork()
 inLayer = LinearLayer(3072)
-hiddenLayer1 = SigmoidLayer(1000)
-hiddenLayer2 = SigmoidLayer(500)
-hiddenLayer3 = SigmoidLayer(250)
+hiddenLayer1 = SigmoidLayer(30)
+hiddenLayer2 = SigmoidLayer(30)
+hiddenLayer3 = SigmoidLayer(30)
 outLayer = LinearLayer(10)
 
 # Add the layers to the neural network
@@ -48,9 +48,20 @@ network.sortModules()
 
 # Load in CIFAR10 dataset using cPcikle and conver to NumPy array
 dataFile1 = open(path.join('cifar-10-batches-py', 'data_batch_1'), 'rd')
-data = cPickle.load(dataFile1)
-images = np.array(data['data'])
-labels = np.array(data['labels'])
+dataFile2 = open(path.join('cifar-10-batches-py', 'data_batch_2'), 'rd')
+dataFile3 = open(path.join('cifar-10-batches-py', 'data_batch_3'), 'rd')
+dataFile4 = open(path.join('cifar-10-batches-py', 'data_batch_4'), 'rd')
+dataFile5 = open(path.join('cifar-10-batches-py', 'data_batch_5'), 'rd')
+
+# Unpack the dataset
+data1 = cPickle.load(dataFile1)
+data2 = cPickle.load(dataFile2)
+data3 = cPickle.load(dataFile3)
+data4 = cPickle.load(dataFile4)
+data5 = cPickle.load(dataFile5)
+
+images = np.append(data1['data'], [data2['data'], data3['data'], data4['data'], data5['data']])
+labels = np.append(data1['labels'], [data2['labels'], data3['labels'], data4['labels'], data5['labels']])
 
 # Construct the supervised data set for learning
 print 'Constructing the Data Set'
